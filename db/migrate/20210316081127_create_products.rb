e3f0870274861929567fc8e5eeb7520278ad1a2d
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-16 18:12:50 +0900

    Active Storageの導入

--- db/migrate/20210316081127_create_products.rb
+++ db/migrate/20210316081127_create_products.rb
@@ -9,7 +9,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id, null: false
       t.integer :delivery_day_id, null: false
       t.integer :price, null: false
-      t.reference :user, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
