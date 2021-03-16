a4270db84ecc9a3891ed184c1b6b14e28700005a
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-17 03:56:59 +0900

    ActiveHash実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ApplicationRecord
-    
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :delivery_fee
+    belongs_to :delivery_day
     belongs_to :user
     has_one_attached :image
+
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 } 
+    validates :delivery_fee_id, numericality: { other_than: 1 } 
+    validates :delivery_day_id, numericality: { other_than: 1 } 
 end
