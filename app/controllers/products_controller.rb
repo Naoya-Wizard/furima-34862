aa209964422f0ab6f2d48592219cb45af8eda54b
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-18 08:58:01 +0900

    情報が保存されるかどうかで条件分岐

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,21 @@ class ProductsController < ApplicationController
     end
 
     def new
+        @product = Product.new
     end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+      end
 end
