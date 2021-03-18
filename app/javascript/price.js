20b07a5c2a96b3f45ade2a00f5154388abea8220
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-18 17:10:20 +0900

    JavaScript実装

---
+++ app/javascript/price.js
@@ -0,0 +1,13 @@
+function price(){
+    const articleText = document.getElementById("item-price")
+    articleText.addEventListener("keyup", () => {
+    const price = articleText.value;
+    const price_fee = Math.floor(price / 10);
+    const price_profit = price - price_fee;
+    const AddTaxPrice = document.getElementById("add-tax-price");
+    AddTaxPrice.innerHTML = `${price_fee}`;
+    const Profit = document.getElementById("profit");
+    Profit.innerHTML = `${price_profit}`;
+  });}
+
+  window.addEventListener('load', price);
