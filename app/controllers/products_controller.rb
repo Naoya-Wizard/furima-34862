a2e315e248d25174b8a907e1c58560dd31cb73ca
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-20 17:07:45 +0900

    ProductControllerにある同じ処理をset_productにまとめる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_product, only: [:show, :edit, :update]
 
     def index
         @product = Product.includes(:user).order("created_at DESC")
@@ -19,15 +20,12 @@ class ProductsController < ApplicationController
     end
 
     def show
-        @product = Product.find(params[:id])
     end
 
     def edit
-        @product = Product.find(params[:id])
     end
 
     def update
-        @product = Product.find(params[:id])
         if @product.update(product_params)
             redirect_to product_path(@product)
         else
@@ -39,5 +37,9 @@ class ProductsController < ApplicationController
 
     def product_params
         params.require(:product).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+    end
+
+    def set_product
+        @product = Product.find(params[:id])
     end
 end
