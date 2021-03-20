781f97b37caa4459c890b5257aa6f46da3d9fa05
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-20 16:52:01 +0900

    ProductControllerのupdateアクションに処理を記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,16 @@ class ProductsController < ApplicationController
     def edit
         @product = Product.find(params[:id])
     end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to product_path(@product)
+        else
+            render :edit
+        end
+    end
+
     private
 
     def product_params
