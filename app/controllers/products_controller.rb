f839141cdbe2b0ab5fd0355fa7d7dd405b6ea0ba
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-20 14:47:55 +0900

    editアクションへのルーティングを設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,5 +26,5 @@ class ProductsController < ApplicationController
 
     def product_params
         params.require(:product).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
-      end
+    end
 end
