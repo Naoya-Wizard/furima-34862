4acdfa106679118daf9546a0624027745b494285
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-24 11:02:15 +0900

    購入画面で入力ミスがあった時にエラーメッセージが出るようにcard.jsに記述の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,22 +20,14 @@ const pay = () => {
             const renderDom = document.getElementById("charge-form");
             const tokenObj = `<input value=${token} name='token' type = 'hidden'> `;
             renderDom.insertAdjacentHTML("beforeend", tokenObj);
-  
+          }
             document.getElementById("card-number").removeAttribute("name");
             document.getElementById("card-cvc").removeAttribute("name");
             document.getElementById("card-exp-month").removeAttribute("name");
             document.getElementById("card-exp-year").removeAttribute("name");    
-  
             document.getElementById("charge-form").submit();
-          }
-          document.getElementById("card-number").removeAttribute("name");
-          document.getElementById("card-cvc").removeAttribute("name");
-          document.getElementById("card-exp-month").removeAttribute("name");
-          document.getElementById("card-exp-year").removeAttribute("name"); 
-           
-          document.getElementById("charge-form").submit();
         });
       });
     };
     
-    window.addEventListener("load", pay);
+window.addEventListener("load", pay);
